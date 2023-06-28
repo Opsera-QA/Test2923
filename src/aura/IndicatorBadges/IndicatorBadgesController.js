51ed1c2c21a991ac602758f0832dded7dbd1a7cf
opsera-user <opsera-user>
2023-06-28 13:27:29 +0000

    Committing selected files as part of metadata restructuring. Commit Number-0

--- src/aura/IndicatorBadges/IndicatorBadgesController.js
+++
@@ -1,9 +0,0 @@
-({
-  doInit : function(component, event, helper) {
-    var sObj = component.get("v.sObjectName");
-    if(sObj){
-      helper.getLabelForRecord(component, sObj);
-    }
-  },
-  //future code here
-})
